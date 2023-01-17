ba1355dcd061dce52b26d89036e08eeb95def499
Andre Rivera <soporte.rpsoft@gmail.com>
2023-01-17 00:11:39 -0500

    Trabajando en el login

---
+++ SmartDoctor/src/app/components/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  public loginForm = new FormGroup({
+    login:    new FormControl(''),
+    password: new FormControl('')
+  });
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+
+  }
+
+}
